40262240e5fe256435ae5d915efacf9fa53cda6a
A350 XWB <hutianxianghtx@outlook.com>
2023-11-06 18:15:54 +0800

    Create JetFlightEnvelope.py

---
+++ hw1/3. Flight Envelope/JetFlightEnvelope.py
@@ -0,0 +1,160 @@
+import math
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
+# ISA数据计算
+def ISA_calc(h):
+    H_lowest, H0, H1, H2, H3, H4, H5, H6, H7 = -610, 0, 11000, 20000, 32000, 47000, 51000, 71000, 84852
+    R, M, Re, Gamma = 8.3144598, 0.0289644, 6371000, 1.4
+    R_special = R / M
+    T0, P0, Rho0, g0 = 288.15, 1013.25, 1.225, 9.80665
+
+    Lambda0to1, Lambda2to3, Lambda3to4, Lambda5to6, Lambda6to7 = -0.0065, 0.0010, 0.0028, -0.0028, -0.0020
+
+    T1to2 = T0 + Lambda0to1 * H1
+    T3 = T1to2 + Lambda2to3 * (H3 - H2)
+    T4to5 = T3 + Lambda3to4 * (H4 - H3)
+    T6 = T4to5 + Lambda5to6 * (H6 - H5)
+
+    P1 = P0 * (1 + Lambda0to1 * (H1 - H0) / T0)**(-g0 / (R_special * Lambda0to1))
+    P2 = P1 * math.exp(-g0 * (H2 - H1) / (R_special * T1to2))
+    P3 = P2 * (1 + Lambda2to3 * (H3 - H2) / T1to2)**(-g0 / (R_special * Lambda2to3))
+    P4 = P3 * (1 + Lambda3to4 * (H4 - H3) / T3)**(-g0 / (R_special * Lambda3to4))
+    P5 = P4 * math.exp(-g0 * (H5 - H4) / (R_special * T4to5))
+    P6 = P5 * (1 + Lambda5to6 * (H6 - H5) / T4to5)**(-g0 / (R_special * Lambda5to6))
+
+    Rho1 = Rho0 * (1 + Lambda0to1 * (H1 - H0) / T0)**(-g0 / (R_special * Lambda0to1) - 1)
+    Rho2 = Rho1 * math.exp(-g0 * (H2 - H1) / (R_special * T1to2))
+    Rho3 = Rho2 * (1 + Lambda2to3 * (H3 - H2) / T1to2)**(-g0 / (R_special * Lambda2to3) - 1)
+    Rho4 = Rho3 * (1 + Lambda3to4 * (H4 - H3) / T3)**(-g0 / (R_special * Lambda3to4) - 1)
+    Rho5 = Rho4 * math.exp(-g0 * (H5 - H4) / (R_special * T4to5))
+    Rho6 = Rho5 * (1 + Lambda5to6 * (H6 - H5) / T4to5)**(-g0 / (R_special * Lambda5to6) - 1)
+
+    if h < H_lowest:
+        print()
+        print('Error: The altitude should between -610m ~ 51000m')
+        print('Your altitude is', h, 'm')
+        print('T P Rho a will be -1')
+        print()
+        T, P, Rho, a, g = -1, -1, -1, -1, -1
+    elif h < H0:
+        print()
+        print('Warning0: The altitude is lower than sea level')
+        print('Your altitude is', h, 'm')
+        print()
+        T = T0 + Lambda0to1 * (h - H0)
+        P = P0 * (1 + Lambda0to1 * (h - H0) / T0)**(-g0 / (R_special * Lambda0to1))
+        Rho = Rho0 * (1 + Lambda0to1 * (h - H0) / T0)**(-g0 / (R_special * Lambda0to1) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H1:
+        T = T0 + Lambda0to1 * (h - H0)
+        P = P0 * (1 + Lambda0to1 * (h - H0) / T0)**(-g0 / (R_special * Lambda0to1))
+        Rho = Rho0 * (1 + Lambda0to1 * (h - H0) / T0)**(-g0 / (R_special * Lambda0to1) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H2:
+        T = T1to2
+        P = P1 * math.exp(-g0 * (h - H1) / (R_special * T1to2))
+        Rho = Rho1 * math.exp(-g0 * (h - H1) / (R_special * T1to2))
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H3:
+        T = T1to2 + Lambda2to3 * (h - H2)
+        P = P2 * (1 + Lambda2to3 * (h - H2) / T1to2)**(-g0 / (R_special * Lambda2to3))
+        Rho = Rho2 * (1 + Lambda2to3 * (h - H2) / T1to2)**(-g0 / (R_special * Lambda2to3) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H4:
+        T = T3 + Lambda3to4 * (h - H3)
+        P = P3 * (1 + Lambda3to4 * (h - H3) / T3)**(-g0 / (R_special * Lambda3to4))
+        Rho = Rho3 * (1 + Lambda3to4 * (h - H3) / T3)**(-g0 / (R_special * Lambda3to4) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H5:
+        T = T4to5
+        P = P4 * math.exp(-g0 * (h - H4) / (R_special * T4to5))
+        Rho = Rho4 * math.exp(-g0 * (h - H4) / (R_special * T4to5))
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h < H6:
+        T = T4to5 + Lambda5to6 * (h - H5)
+        P = P5 * (1 + Lambda5to6 * (h - H5) / T4to5)**(-g0 / (R_special * Lambda5to6))
+        Rho = Rho5 * (1 + Lambda5to6 * (h - H5) / T4to5)**(-g0 / (R_special * Lambda5to6) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    elif h <= H7:
+        T = T6 + Lambda6to7 * (h - H6)
+        P = P6 * (1 + Lambda6to7 * (h - H6) / T6)**(-g0 / (R_special * Lambda6to7))
+        Rho = Rho6 * (1 + Lambda6to7 * (h - H6) / T6)**(-g0 / (R_special * Lambda6to7) - 1)
+        a = math.sqrt(Gamma * R_special * T)
+        g = g0 * (Re / (Re + h))**2
+    else:
+        print()
+        print('Error: The altitude should between -610m ~ 51000m')
+        print('Your altitude is', h, 'm')
+        print('T P Rho a will be -2')
+        print()
+        T, P, Rho, a, g = -2, -2, -2, -2, -2
+
+    return T, P, Rho, a, g
+
+
+def VminVmax_Jet(ALT, W):
+    S, CD0, Epslion = 950, 0.015, 0.05
+    Tsmax, m, CLmax = 12500, 0.6, 2.8
+
+    _, _, Rhos, _, _ = ISA_calc(0)
+    _, _, Rho, _, _ = ISA_calc(ALT * 0.3048)
+    Rhos*=0.0019
+    Rho*=0.0019
+
+    roots = np.roots([1/2 * Rho * S * CD0, 0, -Tsmax * (Rho / Rhos)**m, 0, 2 * Epslion * W**2/ Rho / S])
+    r = sorted(x.real for x in roots)
+    Vn1, Vn2, Vmin, Vmax = r
+
+    return Vmin, Vmax
+
+def FlightCeiling_Jet(ALT, W):
+    S, CD0, Epslion = 950, 0.015, 0.05
+    Tsmax, m, CLmax = 12500, 0.6, 2.8
+
+    _, _, Rhos, _, _ = ISA_calc(0)
+    _, _, Rho, _, _ = ISA_calc(ALT * 0.3048)
+    Rhos*=0.0019
+    Rho*=0.0019
+
+    ret = 2 * W * math.sqrt(Epslion * CD0) - Tsmax * (Rho / Rhos)**m
+
+    return ret
+
+
+'''
+W: 飞机重量-三个不同的值 ( 磅 | lbs )
+'''
+W = [63000, 73000, 83000]
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+i = 0
+for W_i in W:
+    Hmax = fsolve(FlightCeiling_Jet, [10000], W_i)
+    Vhmax, Vh = VminVmax_Jet(Hmax, W_i)
+    H = np.arange(0, Hmax, 10, float)
+    Vmin = np.zeros(H.size)
+    Vmax = np.zeros(H.size)
+    j = 0
+    for H_j in H:
+        Vmin[j], Vmax[j] = VminVmax_Jet(H_j, W_i)
+        j+=1
+    ax.plot(Vmin, H, c='magenta')
+    ax.plot(Vmax, H, c='blue')
+    ax.scatter(Vhmax, Hmax, s=70, c='red', marker='8', alpha=0.5)
+    i+=1
+ax.set_xlabel('V (ft/s)')
+ax.set_ylabel('H (ft)')
+ax.text(870, 52000, 'W1=63000')
+ax.text(820, 47000, 'W2=73000')
+ax.text(770, 42000, 'W3=83000')
+ax.grid(True)
+plt.show()
